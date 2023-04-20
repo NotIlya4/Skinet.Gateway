45b027bb79be333f4eef1d6ebc1cad7f60146722
NotIlya <secretman44444@gmail.com>
2023-04-20 12:17:03 +0300

    Add more simple http client

---
+++ src/Infrastructure/Client/ServiceBadResponseException.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Infrastructure.Client;
+
+public class ServiceBadResponseException : Exception
+{
+    public string Title { get; }
+    public string Detail { get; }
+    public int StatusCode { get; }
+    
+    private ServiceBadResponseException(string msg, string title, string detail, int statusCode) : base(msg)
+    {
+        Title = title;
+        Detail = detail;
+        StatusCode = statusCode;
+    }
+
+    public static async Task<ServiceBadResponseException> Create(HttpResponseMessage message)
+    {
+        JObject jObject = JObject.Parse(await message.Content.ReadAsStringAsync());
+        string title = jObject["title"]!.Value<string>()!;
+        string detail = jObject["detail"]!.Value<string>()!;
+        string msg = $"{title}: {detail}";
+        return new ServiceBadResponseException(msg, title, detail, (int)message.StatusCode);
+    }
+}
---
+++ src/Infrastructure/Client/ServiceHttpClient.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using Newtonsoft.Json.Linq;
+using UriQueryStringComposer;
+
+namespace Infrastructure.Client;
+
+public class ServiceHttpClient : IServiceHttpClient
+{
+    private readonly HttpClient _client;
+
+    public ServiceHttpClient(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<T> Get<T>(Uri url, object? queryParams = null)
+    {
+        HttpResponseMessage response = await _client.GetAsync(QueryStringComposer.Compose(url, queryParams));
+        await EnsureSuccess(response);
+        return await ParseResponseBody<T>(response);
+    }
+
+    public async Task Get(Uri url, object? queryParams = null)
+    {
+        HttpResponseMessage response = await _client.GetAsync(QueryStringComposer.Compose(url, queryParams));
+        await EnsureSuccess(response);
+    }
+
+    public async Task<string> GetRaw(Uri url, object? queryParams = null)
+    {
+        HttpResponseMessage response = await _client.GetAsync(QueryStringComposer.Compose(url, queryParams));
+        await EnsureSuccess(response);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public async Task<T> Post<T>(Uri url, object? body = null)
+    {
+        HttpContent? requestBody = MakeHttpContent(body);
+        HttpResponseMessage response = await _client.PostAsync(url, requestBody);
+        await EnsureSuccess(response);
+        return await ParseResponseBody<T>(response);
+    }
+
+    public async Task Post(Uri url, object? body = null)
+    {
+        HttpContent? requestBody = MakeHttpContent(body);
+        HttpResponseMessage response = await _client.PostAsync(url, requestBody);
+        await EnsureSuccess(response);
+    }
+
+    public async Task<string> PostRaw(Uri url, object? body = null)
+    {
+        HttpContent? requestBody = MakeHttpContent(body);
+        HttpResponseMessage response = await _client.PostAsync(url, requestBody);
+        await EnsureSuccess(response);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    private async Task EnsureSuccess(HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+        {
+            throw await ServiceBadResponseException.Create(response);
+        }
+    }
+    
+    private StringContent? MakeHttpContent(object? body)
+    {
+        if (body is null)
+        {
+            return null;
+        }
+        JObject bodyJObject = JObject.FromObject(body);
+        return new StringContent(bodyJObject.ToString(), Encoding.UTF8, "application/json");
+    }
+    
+    private async Task<T> ParseResponseBody<T>(HttpResponseMessage response)
+    {
+        return JObject.Parse(await response.Content.ReadAsStringAsync()).ToObject<T>() ??
+               throw new InvalidOperationException("Failed to parse response from account service");
+    }
+}
